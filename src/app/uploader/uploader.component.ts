af62deb65bf910b15ae6b419b918a133eef0ce16
Paweł Łaba <laba.pawel@wsi.edu.pl>
2019-11-18 06:10:40 +0100

    progress

--- src/app/uploader/uploader.component.ts
+++ src/app/uploader/uploader.component.ts
@@ -21,6 +21,18 @@ export class UploaderComponent implements OnInit {
   ngOnInit() {
     this.interv = interval(1000).subscribe(e => {
 
+        let wysylam1 = this.sendFiles.filter(e => e.status == 1).length;
+        let wysylam2 = this.sendFiles.filter(e => e.status == 2).length;
+        // console.log(this.sendFiles);
+        
+        if(wysylam2 < this.maxSendFiles  && wysylam1 == 0){
+          let wyslij = this.sendFiles.filter(e => e.status == 0);
+          if(wyslij.length >0)
+          {
+            // console.log(wyslij);
+            this.sendFile();
+          }
+        }
     });
   }
 
@@ -28,9 +40,11 @@ export class UploaderComponent implements OnInit {
     clearInterval(this.interv);
   }
 
-  sendFile(file) {
+  sendFile() {
+    let wyslij = this.sendFiles.filter(e => e.status == 0);
+    wyslij[0].status = 1;
     let formData = new FormData();
-    formData.append('file', file);
+    formData.append('file', wyslij[0].file);
 
     this.http.post(this.url, formData, {
       reportProgress: true,
@@ -40,14 +54,24 @@ export class UploaderComponent implements OnInit {
         if (events.type === HttpEventType.UploadProgress) {
 
 
-          //  this.progressValue[i].progress = Math.round(events.loaded / events.total * 100);
-          // console.log(this.progressValue[0].filesize);
-          // console.log(events.total);
-
-          // console.log(this.progressValue.filter(e => {e.filesize == events.total} ));
-          // this.progressValue.filter(e => e.filesize == events.total )[0].progress = Math.round(events.loaded / events.total * 100);
+          let prc = Math.round(events.loaded / events.total * 100);
+          let fileEx = this.sendFiles.filter(e =>  e.filesize == events.total );
+          console.log(fileEx);
+          if(fileEx.length == 0)
+          {
+            fileEx = this.sendFiles.filter(e => e.status == 1);
+            console.log(fileEx);
+            if(fileEx.length == 1)
+            {
+              fileEx[0].filesize =  events.total;
+              fileEx[0].status = 2;
+              fileEx[0].progress = prc;
+            }
+          } else {
+            fileEx[0].progress = prc;
+          };
         } else if (events.type === HttpEventType.Response) {
-          //       console.log(events.body);
+                 console.log(events);
           //          alert('SUCCESS !!');
         }
 
@@ -57,17 +81,17 @@ export class UploaderComponent implements OnInit {
   }
 
   bindings(val) {
-    this.files = val.srcElement.files;
 
 
-    for (var i = 0; i < this.files.length; i++) {
-      let fileSendCount = this.sendFiles.filter(e => { e.filename == this.files[i].name });
+    for (var i = 0; i < val.srcElement.files.length; i++) {
+      let fileSendCount = this.sendFiles.filter(e => { e.filename == val.srcElement.files[i].name });
       if (fileSendCount.length == 0) {
         this.sendFiles.push({
           progress: 0,
           status: 0, // 0 do wysłania, 1 wysyłam start, 2 wysyłam, 3 koniec ok, 4 koniec nok
           filesize: 0,
-          filename: this.files[i].name
+          file: val.srcElement.files[i],
+          filename: val.srcElement.files[i].name
         });
       }
 
